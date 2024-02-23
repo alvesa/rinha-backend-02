53abae583512f9ee453a91acd2a2b4497f0deaa0
Andre Alves <andresistinfo@gmail.com>
2024-02-23 12:33:13 +0000

    Minor fix

--- rinha-backend-api/Controllers/Response/ExtratoResposta.cs
+++ rinha-backend-api/Controllers/Response/ExtratoResposta.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using rinha_backend_api.Controllers.Request;
 
 namespace rinha_backend_api.Controllers.Response
@@ -5,19 +6,24 @@ namespace rinha_backend_api.Controllers.Response
     public class ExtratoResposta
     {
         public SaldoResponse Saldo { get; set; }
+        [JsonPropertyName("ultimas_transacoes")]
+
         public IEnumerable<UltimasTransacoesResposta> UltimasTransacoes { get; set; }
     }
 
     public class SaldoResponse {
         public long Total { get; set; }
+        [JsonPropertyName("data_extrato")]
         public DateTime Data_Extrato { get; set; }
         public long Limite { get; set; }
     }
 
     public class UltimasTransacoesResposta {
         public long Valor { get; set; }
+        [JsonPropertyName("tipo")]
         public char TipoTransacao { get; set; }
         public string Descricao { get; set; }
+        [JsonPropertyName("realizada_em")]
         public DateTime? Realizada_Em { get; set; }
     }
 }
