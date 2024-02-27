00a56da6808e9c299a29ea9367df2da66632cc9e
Andre Alves <andresistinfo@gmail.com>
2024-02-27 05:28:42 +0000

    request with custom validation

--- rinha-backend-api/Controllers/Request/TransacaoRequest.cs
+++ rinha-backend-api/Controllers/Request/TransacaoRequest.cs
@@ -1,5 +1,6 @@
 
 using System.ComponentModel.DataAnnotations;
+using rinha_backend_api.Controllers.Helper;
 using rinha_backend_api.Controllers.Request;
 
 namespace Controllers.Request;
@@ -7,14 +8,15 @@ namespace Controllers.Request;
 public record TransacaoRequisicao {
 
     [Required(ErrorMessage = "Campo obrigatorio")]
+    // [CustomValue("Valor invalido")]
     public long Valor { get; set; }
 
     [EnumDataType(typeof(TipoTransacao), ErrorMessage = "Tipo de trasacao nao valida")]
     [Required(ErrorMessage = "Campo obrigatorio")]
     public string? Tipo{ get; set; }
 
-    [MinLength(1, ErrorMessage = "Tamanho maximo e 1")]
-    [MaxLength(10, ErrorMessage = "Tamanho maximo e 10")]
+    [CustomMinLength(1,"Tamanho minimo e de 1 caracter")]
+    [CustomMaxLength(10,"Tamanho maximo e de 10 caracteres")]
     [Required(ErrorMessage = "Campo obrigatorio")]
     public string? Descricao { get; set; }
 }
